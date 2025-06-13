c2d8144d75f88f58a79613c263cc5c0f417a39a2
UkkyNtem <ukementem32@gmail.com>
2025-06-13 14:44:22 +0300

    Update lambda_function.py

--- backend/upload_note/lambda_function.py
+++ backend/upload_note/lambda_function.py
@@ -12,7 +12,7 @@ table_name = 'SickNotes'
 table = dynamodb.Table(table_name)
 
 BUCKET_NAME = 'sicknote-vault-bucket'
-FROM_EMAIL = '@example.com'
+FROM_EMAIL = 'ukeme@example.com'
 
 def lambda_handler(event, context):
     try:
